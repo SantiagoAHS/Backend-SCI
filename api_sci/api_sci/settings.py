3d0678ef3f7dd59e6f7bcc69cd9649b09ebe7d46
SantiagoAHS <lionelsanti2018@gmail.com>
2026-02-18 17:12:03 -0600

    Login num_emp + pwd

--- api_sci/api_sci/settings.py
+++ api_sci/api_sci/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    "corsheaders",
 
     'user',
     'areas',
@@ -51,7 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'api_sci.urls'
 
--- api_sci/user/views.py
+++ api_sci/user/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        numero_empleado = request.data.get("numero_empleado")
+        password = request.data.get("password")
+
+        if not numero_empleado or not password:
+            return Response({"error": "Debe enviar numero_empleado y password"}, status=400)
+
+        try:
+            user_obj = User.objects.get(numero_empleado=numero_empleado)
+        except User.DoesNotExist:
+            return Response({"error": "Credenciales inválidas"}, status=400)
+
+        user = authenticate(
+            request,
+            username=user_obj.username,  # usamos username interno
+            password=password
+        )
+
+        if user is not None:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                "user": UserSerializer(user).data,
+                "token": token.key
+            })
+
+        return Response({"error": "Credenciales inválidas"}, status=400)
