ac1f989ff1e63c8389c2ab42db4f95b489b92ff8
SantiagoAHS <lionelsanti2018@gmail.com>
2026-03-02 15:54:45 -0600

    Activos module stadistics

--- api_sci/activos/urls.py
+++ api_sci/activos/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TipoActivoListCreateView, ActivoCreateView, TipoActivoDeleteView, ActivoListView
+from .views import TipoActivoListCreateView, ActivoCreateView, TipoActivoDeleteView, ActivoListView, ActivoDeleteView, DashboardStatsView
 
 urlpatterns = [
     path('tipos-activo/', TipoActivoListCreateView.as_view(), name='tipos-activo'),
     path('tipos-activo/<int:pk>/', TipoActivoDeleteView.as_view(), name='tipo-activo-delete'),
     path('activos/', ActivoCreateView.as_view(), name='crear-activo'),
     path('activos/list/', ActivoListView.as_view(), name='activo-list'),
+    path('activos/<int:pk>/delete/', ActivoDeleteView.as_view(), name='activo-delete'),
+    path("dashboard/stats/", DashboardStatsView.as_view(), name="dashboard-stats"),
 ]
--- api_sci/activos/views.py
+++ api_sci/activos/views.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import TipoActivo, Activo
 from .serializers import TipoActivoSerializer, ActivoCreateSerializer, ActivoListSerializer
+from rest_framework.views import APIView
+from django.db.models import Count
 
 # Solo admin puede crear
 class TipoActivoCreateView(generics.CreateAPIView):
@@ -72,3 +74,62 @@ class ActivoListView(generics.ListAPIView):
     queryset = Activo.objects.all()
     serializer_class = ActivoListSerializer
     permission_classes = [IsAuthenticated]
+
+class ActivoDeleteView(generics.DestroyAPIView):
+    queryset = Activo.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        if request.user.rol != "admin":
+            return Response(
+                {"error": "No autorizado"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        instance = self.get_object()
+        instance.delete()  # eliminación física real
+
+        return Response(
+            {"detail": "Activo eliminado correctamente."},
+            status=status.HTTP_204_NO_CONTENT
+        )
+    
+class DashboardStatsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+
+        # Total activos
+        total_activos = Activo.objects.count()
+
+        # Conteo por estado (usando tus choices reales)
+        activos_por_estado = (
+            Activo.objects
+            .values("estado")
+            .annotate(total=Count("id"))
+        )
+
+        # Conteo por tipo de activo
+        activos_por_tipo = (
+            Activo.objects
+            .values("tipo_activo__nombre")
+            .annotate(total=Count("id"))
+        )
+
+        # KPIs específicos
+        asignados = Activo.objects.filter(estado="asignado").count()
+        mantenimiento = Activo.objects.filter(estado="mantenimiento").count()
+        baja = Activo.objects.filter(estado="baja").count()
+        disponibles = Activo.objects.filter(estado="disponible").count()
+
+        return Response({
+            "kpis": {
+                "total": total_activos,
+                "asignados": asignados,
+                "mantenimiento": mantenimiento,
+                "baja": baja,
+                "disponibles": disponibles
+            },
+            "por_estado": list(activos_por_estado),
+            "por_tipo": list(activos_por_tipo),
+        })
