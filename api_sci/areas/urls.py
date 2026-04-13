4615495f5752e38f62ead791e21e8d826c6ba068
SantiagoAHS <lionelsanti2018@gmail.com>
2026-04-13 14:11:22 -0600

    Areas module: actualizacion de estructuras v2.0

--- api_sci/areas/urls.py
+++ api_sci/areas/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import AreaCreateView, AreaListView, AreaUpdateView, AreaDeleteView
+from .views import (
+    AreaCreateView,
+    AreaListView,
+    AreaUpdateView,
+    AreaDeleteView
+)
 
 urlpatterns = [
+
+    # ==============================
+    # GESTIÓN DE ÁREAS
+    # ==============================
     path("areas/", AreaCreateView.as_view(), name="area-create"),
     path("areas/list/", AreaListView.as_view(), name="area-list"),
     path("areas/<int:pk>/", AreaUpdateView.as_view(), name="area-update"),
